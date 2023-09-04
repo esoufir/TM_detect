7e2f97295d04d0998db7b371d5bd3c0e5248464f
esoufir <emmas@Mitochondrie>
2023-09-04 17:21:19 +0200

    More geometrical function

--- src/Vector.py
+++ src/Vector.py
@@ -10,6 +10,7 @@ class Vector:
         self.z=z
 
 # TODO: Mieux encapsuler avec Point et tout
+# TODO: faire que le demi cercle
 def find_points(n_points, center_coordinates):
     x,y,z = center_coordinates
     points = []
@@ -19,7 +20,7 @@ def find_points(n_points, center_coordinates):
         if k==1 or k==n_points:
             phi = 0
         else :
-            phi = (phi + (3.6/math.sqrt(n_points) * (1/math.sqrt(1-h*h)))) % (2*math.pi)
+            phi = (phi + (1.8/math.sqrt(n_points) * (1/math.sqrt(1-h*h)))) % (2*math.pi)
         x = math.sin(phi) * math.sin(theta)
         y = math.cos(theta)
         z = math.cos(phi) * math.sin(theta)
@@ -32,22 +33,27 @@ def find_director_vector(point, center_coordinate):
     x2,y2,z2 = center_coordinate
     return (x2-x1, y2 - y1, z2 - z1)
 
+def find_plane_equation(point, center_coordinate):
+    
+
 def find_normal_plan(director_vector):
     """Returns 2 normal vectors making a plan orthogonal to the director vector"""
     x,y,z=director_vector
-    return ( (-y,x,0) , (-z,0,x))
+    return ( np.array([-y,x,0]) , np.array([-z,0,x]))
   
 
 if __name__ == '__main__':
     print("Test vector")
-    directions = find_points(200, (0,0,0))
+    directions = find_points(20, (0,0,0))
   
+    # Plot points check
     """fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
     for d in directions:
         ax.scatter3D(d[0], d[1], d[2])
     plt.show()"""
+
 
 
 
