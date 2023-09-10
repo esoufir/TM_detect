72cd9a9086050bdfc89f835f7c9d54161ec811d7
esoufir <emmas@Mitochondrie>
2023-09-10 09:54:31 +0200

    Correct bug on planes

--- src/Protein.py
+++ src/Protein.py
@@ -2,50 +2,52 @@ import argparse
 from Bio.PDB import *
 import numpy as np
 import pymol
-import subprocess
-import math
 
-from AminoAcid import * # je crois que c'est une mauvaise pratique mdr
+
+from AminoAcid import *  # je crois que c'est une mauvaise pratique mdr
 import Vector
+
 
 class Protein:
     def __init__(self, name):
         self.name = name
-        self.mass_center = Vector.Point(0,0,0)
+        self.mass_center = Vector.Point(0, 0, 0)
         self.amino_acid_sequence = []
         self.best_positions = []
-    
+
     def get_amino_acid_sequence(self):
         return self.amino_acid_sequence
-    
+
     def add_to_amino_acid_sequence(self, new_amino_acid):
         self.amino_acid_sequence.append(new_amino_acid)
-    
+
     def set_mass_center(self, new_mass_center):
-        self.mass_center=new_mass_center
-    
+        self.mass_center = new_mass_center
+
     def print_protein(self):
         for residue in self.amino_acid_sequence:
             print(residue.get_code(), end=" ")
         print()
-    
+
     def find_best_axis(self):
         best_axis_val = 0
         best_axis = None
         for axis in self.best_positions:
             if axis.best_number_hits > best_axis_val:
                 best_axis_val = axis.best_number_hits
-                best_axis = axis  
+                best_axis = axis
         return best_axis
 
 
-# TODO: ? trouver avant les 4 points les plus éloignés et si on regarde dans des tranches en dehors de ces points => break   
+# TODO: ? trouver avant les 4 points les plus éloignés et si on regarde dans des tranches en dehors de ces points =>
+#  break
 
-# TODO: Méthode qui calcule le centre de masse:,  a voir si on le calcule à la main ou pas et si on la met dans la classe protéine oupas
+# TODO: Méthode qui calcule le centre de masse:,  a voir si on le calcule à la main ou pas et si on la met dans la
+#  classe protéine oupas
 def compute_mass_center(chain):
     result = chain.center_of_mass()
-    x,y,z = result[0], result[1], result[2]
-    return x,y,z
+    x, y, z = result[0], result[1], result[2]
+    return x, y, z
 
 
 # A mettre ailleurs : 
@@ -55,7 +57,7 @@ def check_input_file(input_file):
         header_dict = parse_pdb_header(handle)
 
 
-def caculate_solvant_accessibility(structure, input_file,protein): # a voir pour identifier le bon aa
+def caculate_solvant_accessibility(structure, input_file, protein):  # a voir pour identifier le bon aa
     # Using DSSP
     print("Computing solvant accessibility...")
     model = structure[0]
@@ -63,13 +65,12 @@ def caculate_solvant_accessibility(structure, input_file,protein): # a voir pour
     # Pour chaque acide aminé, on a ASA = Accessible Surface Area
     for i in range(len(dssp.keys())):
         a_key = list(dssp.keys())[i]
-        protein.get_amino_acid_sequence()[i].set_asa(dssp[a_key][3])#pas sure pour i"""
-    
-
+        protein.get_amino_acid_sequence()[i].set_asa(dssp[a_key][3])  # pas sure pour i"""
 
 
 def parse_pdb(input_file):
-    #TODO: Parse the PDB File to get only one chain (pour le moment, cf extension) and get residues and atoms and put them in objects
+    # TODO: Parse the PDB File to get only one chain (pour le moment, cf extension) and get residues and atoms and
+    #  put them in objects
     print("Parsing the PDB file ...")
     p = PDBParser()
     # TODO: Adapt le X
@@ -82,22 +83,21 @@ def parse_pdb(input_file):
         print(f"Found {len(model)} chains in structure...")
         for chain in model:
             xm, ym, zm = compute_mass_center(chain)
-            protein.mass_center = Vector.Point(xm,ym,zm)  
+            protein.mass_center = Vector.Point(xm, ym, zm)
             for residue in chain:
                 # Getting the C-alpha
                 if residue.has_id("CA"):
                     atom = residue['CA']
-                    x,y,z = atom.get_coord()
+                    x, y, z = atom.get_coord()
                     # Creating and setting the AminoAcid object
-                    new_amino_acid = AminoAcid(code=residue.get_resname(), id = id_amino_acid,x=x,y=y,z=z)
+                    new_amino_acid = AminoAcid(code=residue.get_resname(), id=id_amino_acid, x=x, y=y, z=z)
                     # Linking the amino acid to its protein object
                     protein.add_to_amino_acid_sequence(new_amino_acid=new_amino_acid)
-                    id_amino_acid+=1
+                    id_amino_acid += 1
     # Compute the the solvant accessibility and set it for each amino acid of the protein 
     # TODO: Ne garder que les résidus accessibles au solvant, donc ne créer que ces objets
     caculate_solvant_accessibility(structure, input_file=input_file, protein=protein)
     return protein
-
 
 
 # TODO :All the logging file https://docs.python.org/3/howto/logging.html
@@ -105,61 +105,82 @@ def parse_pdb(input_file):
 if __name__ == '__main__':
     # TODO: Compléter cela
     parser = argparse.ArgumentParser(
-                    prog='ProgramName',
-                    description='What the program does',
-                    epilog='Text at the bottom of help')
-    parser.add_argument('filename',help="A PDB file...")
-    #parser.add_argument('-c', '--count')      # option that takes a value
+        prog='ProgramName',
+        description='What the program does',
+        epilog='Text at the bottom of help')
+    parser.add_argument('filename', help="A PDB file...")
+    # parser.add_argument('-c', '--count')      # option that takes a value
     args = parser.parse_args()
-    print(f"Command : Protein.py {args.filename}")# to adapt
-    
+    print(f"Command : Protein.py {args.filename}")  # to adapt
+
     protein = parse_pdb(args.filename)
     # Number of points
-    n = 40
-    directions = Vector.find_points(n*2, protein.mass_center)
+    n = 5
+    directions = Vector.find_points(n * 2, protein.mass_center)
     print("Calculating the planes... ")
     # For each direction...
     for d in directions:
-        point  = d
+        point = d
         # Find the normal vector
         normal = Vector.find_director_vector(point=d, center_coordinate=protein.mass_center)
         # Construction of the two planes representing the membrane : 
         plane1 = Vector.Plane(point=point, normal=normal)
         plane2 = plane1.complementary(14)  # 14 Angstrom, à voir pour la modularité TODO
-
-        axis = Vector.Axis(p1=plane1,p2=plane2)
+        best_axis_tmp = None
+        axis = Vector.Axis(p1=plane1, p2=plane2)
         # Looking above : 
-        while axis.explore_axe(protein.amino_acid_sequence) == True:
-            #Sliding the planes if necessary 
-            plane1.slide_plane(1) 
-            plane2.slide_plane(1)# adapte la gap je pense
         
+        while axis.explore_axe(protein.amino_acid_sequence) == True:
+            # Sliding the planes if necessary
+            axis.plane1.slide_plane(1)
+            axis.plane2.slide_plane(1)  # adapte la gap je pense
+            res_explore = axis.explore_axe(protein.amino_acid_sequence)
+            if best_axis_tmp is None or axis.best_number_hits > best_axis_tmp.best_number_hits : 
+                best_axis_tmp = axis
+                print("AXIS IMPROVED ABOVE")
+
+        
+        # Keeping in mind the best axis found yet : 
+        # ???
+        print("BEST AXIS AFTER ABOVE EXPLORATION", best_axis_tmp)
         # Resetting start positions
         plane1 = Vector.Plane(point=point, normal=normal)
         plane2 = plane1.complementary(14)  # 14 Angstrom, à voir pour la modularité TODO
-        #Looking below : 
-        while axis.explore_axe(protein.amino_acid_sequence) == True:
-            #Sliding the planes if necessary 
-            plane1.slide_plane(-1) 
-            plane2.slide_plane(-1)
-        protein.best_positions.append(axis)
+        axis = Vector.Axis(p1=plane1, p2=plane2) # pas sure de si faut le remettre
+
+
+
+        # Looking below :
+        while axis.explore_axe(protein.amino_acid_sequence):
+            # Sliding the planes if necessary
+            axis.plane1.slide_plane(-1)
+            axis.plane2.slide_plane(-1)
+            if best_axis_tmp is None or axis.best_number_hits > best_axis_tmp.best_number_hits : 
+                best_axis_tmp = axis
+                print("AXIS IMPROVED BELOW")
         
-        #print(f"With this axis, {axis.best_number_hits} hits for {axis.best_number_aa} amino acids in-between, so a ratio of : {axis.best_number_hits/axis.best_number_aa}")
-   
+        print("BEST AXIS AFTER below EXPLORATION", best_axis_tmp)
+
+        #print("BEST AXIS WAS :", best_axis_tmp)
+        # Saving the best poisiton for the axis
+        protein.best_positions.append(best_axis_tmp)
+
+        # print(f"With this axis, {axis.best_number_hits} hits for {axis.best_number_aa} amino acids in-between, so a ratio of : {axis.best_number_hits/axis.best_number_aa}")
+
     best_axis = protein.find_best_axis()
-    print("BEST AXIS IS ", best_axis)
+    print("BEST AXIS OVERALL IS ", best_axis, "WITH PLANES = ", best_axis.plane1, best_axis.plane2)
 
     plane1 = best_axis.plane1
     plane2 = best_axis.plane2
 
     # TODO: Faire l'optimisation de la largeur de la membrane
     pymol.finish_launching()
-    pymol.cmd.load("../data/1prn.pdb", "molecule_name")
+    pymol.cmd.load(args.filename, "molecule_name")
 
     # Create two dummy objects
     # TODO : Adapt
-    x_min, x_max = -20, 20
-    y_min, y_max = -20, 20
+    x_min, x_max = -10, 10
+    y_min, y_max = -10, 10
 
     # Define the step size for sampling points
     step = 2
@@ -168,29 +189,30 @@ if __name__ == '__main__':
     points_on_plane = []
 
     # Generate points on the plane
-    for x in np.arange(x_min, x_max + step, step):
-        for y in np.arange(y_min, y_max + step, step):
-            # Calculate z coordinate using the plane equation
-            z = (-plane1.a * x - plane1.b * y - plane1.d) / plane1.c
-            points_on_plane.append((x, y, z))
-            
+    for ax in protein.best_positions:
+        for x in np.arange(x_min, x_max + step, step):
+            for y in np.arange(y_min, y_max + step, step):
+                # Calculate z coordinate using the plane equation
+                z = (-ax.plane1.a * x - ax.plane1.b * y - ax.plane1.d) / ax.plane1.c
+                points_on_plane.append((x, y, z))
+
     for idx, point in enumerate(points_on_plane):
         x, y, z = point
         atom_name = f"dummy_{idx}"
         pymol.cmd.pseudoatom(atom_name, pos=[x, y, z], color="red")
         pymol.cmd.show("spheres", f"dummy_{idx}")
 
-    
     # Initialize an empty list to store the points
     points_on_plane = []
 
     # Generate points on the plane
-    for x in np.arange(x_min, x_max + step, step):
-        for y in np.arange(y_min, y_max + step, step):
-            # Calculate z coordinate using the plane equation
-            z = (-plane2.a * x - plane2.b * y - plane2.d) / plane2.c
-            points_on_plane.append((x, y, z))
-            
+    for ax in protein.best_positions:
+        for x in np.arange(x_min, x_max + step, step):
+            for y in np.arange(y_min, y_max + step, step):
+                # Calculate z coordinate using the plane equation
+                z = (-ax.plane2.a * x - ax.plane2.b * y - ax.plane2.d) / ax.plane2.c
+                points_on_plane.append((x, y, z))
+
     for idx, point in enumerate(points_on_plane):
         x, y, z = point
         atom_name = f"dummy_{idx}"
@@ -204,4 +226,3 @@ if __name__ == '__main__':
 
     # Save an image if needed
     pymol.cmd.png("planes.png")
-
--- src/Vector.py
+++ src/Vector.py
@@ -1,18 +1,18 @@
 import numpy as np
 import math
-import copy 
+import copy
 import subprocess
 from Bio.PDB import PDBParser
 from Bio.PDB.DSSP import DSSP
 
-#problème avec matplolib dans le venv
+
 class Vector:
-    def __init__(self, x,y,z):
-        self.coordinates = np.array([x,y,z])
-    
+    def __init__(self, x, y, z):
+        self.coordinates = np.array([x, y, z])
+
     def get_coordinates(self):
         return self.coordinates
-    
+
     def get_x(self):
         return self.coordinates[0]
 
@@ -21,21 +21,21 @@ class Vector:
 
     def get_z(self):
         return self.coordinates[2]
-    
-    def norma(self):
-        return (math.sqrt(self.x**2 + self.y **2 + self.z **2))
 
+    def norma(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
 
 class Point:
-    def __init__(self, x,y,z):
-        self.coordinates = np.array([x,y,z])
-    
+    def __init__(self, x, y, z):
+        self.coordinates = np.array([x, y, z])
+
     def __str__(self):
         return f"Point({self.coordinates[0]:.3},{self.coordinates[1]:.3},{self.coordinates[2]:.3})"
-    
+
     def dot_product(self, normal):
-        return (-self.coordinates.dot(normal.get_coordinates()))
+        return -self.coordinates.dot(normal.get_coordinates())
+
     def get_x(self):
         return self.coordinates[0]
 
@@ -44,25 +44,24 @@ class Point:
 
     def get_z(self):
         return self.coordinates[2]
+
     def get_coordinates(self):
         return self.coordinates
-    
 
 
 class Plane:
-    def __init__(self, point:Point, normal:Vector):
-        self.point=point
-        self.normal=normal
+    def __init__(self, point: Point, normal: Vector):
+        self.point = point
+        self.normal = normal
         self.a = normal.get_coordinates()[0]
         self.b = normal.get_coordinates()[1]
         self.c = normal.get_coordinates()[2]
-        #self.d = -self.point.dot_product(self.normal)
-        self.d = -self.a * self.point.get_x() - self.b * self.point.get_y() - self.c * self.point.get_z() # https://mathworld.wolfram.com/Plane.html
+        # self.d = -self.point.dot_product(self.normal)
+        self.d = -self.a * self.point.get_x() - self.b * self.point.get_y() - self.c * self.point.get_z()  # https://mathworld.wolfram.com/Plane.html
 
     def __str__(self):
         return f"Plane equation is : {self.a}x + {self.b}y +{self.c}z + {self.d} = 0\n"
-    
-    
+
     def complementary(self, gap):
         # gap in Angstrom
         # Copying the object
@@ -73,121 +72,129 @@ class Plane:
     def slide_plane(self, sliding_window):
         # slide the plane 
         self.d += sliding_window
-    
+
     # http://mathonline.wikidot.com/point-normal-form-of-a-plane
-    #TODO: Ces 2 fonctions
-    def is_above(self,point):
+    # TODO: Ces 2 fonctions
+    def is_above(self, point):
         # Return true if the point is located over the plane (self)
-        return True if (self.a * point.get_x() +self.b * point.get_y() + self.c * point.get_z() + self.d ) > 0  else False
-    
+        return True if (
+                                   self.a * point.get_x() + self.b * point.get_y() + self.c * point.get_z() + self.d) > 0 else False
+
     def is_below(self, point):
         # Return true if the point is located under the plane (self)
-        return True if (self.a * point.get_x() +self.b * point.get_y() + self.c * point.get_z() + self.d )   < 0 else False
+        return True if (
+                                   self.a * point.get_x() + self.b * point.get_y() + self.c * point.get_z() + self.d) < 0 else False
 
-    
 
 class Axis:
-    def __init__(self,p1,p2):
+    def __init__(self, p1, p2):
         self.plane1 = p1
         self.plane2 = p2
         self.best_number_hits = 0
         self.best_number_aa = 0
-    
+
     def __str__(self):
-        return (f"AXIS with best nb hits has {self.best_number_hits}")
-    
-    def explore_axe(self,amino_acid_sequence): 
-        # TODO: Quand la tranche qu'on regarde ne contient aucun Calpha => BREAk
-        #Je crois que c'est ce qui est fait vu qu'on retourne faux des qu'on a que des trucs qui correspondent pas : a voir ce qui se passe si on tombe sur des truca mauvais de base
+        return f"AXIS with best nb hits has {(self.best_number_hits/self.best_number_aa)*100:.3f}%"
+
+    def explore_axe(self, amino_acid_sequence):
+        # TODO: Quand la tranche qu'on regarde ne contient aucun Calpha => BREAk Je crois que c'est ce qui est fait
+        #  vu qu'on retourne faux des qu'on a que des trucs qui correspondent pas : a voir ce qui se passe si on
+        #  tombe sur des truca mauvais de base
         in_between_planes = []
         number_atoms_in_between = 0
         number_atoms_hits = 0
+        best_axis = None
         for aa in (amino_acid_sequence):
-            if (self.plane1.is_below(aa.point) and self.plane2.is_above(aa.point)) or self.plane2.is_below(aa.point) and self.plane1.is_above(aa.point):
-                number_atoms_in_between+=1
-                #print("ASA", aa.asa )
-                if aa.asa > 0.30 and aa.is_hydrophobic == True :
+            if (self.plane1.is_below(aa.point) and self.plane2.is_above(aa.point)) or self.plane2.is_below(
+                    aa.point) and self.plane1.is_above(aa.point):
+                number_atoms_in_between += 1
+                # If the amino acid is hydrophobic and exposed, then it's a hit
+                if aa.asa > 0.30 and aa.is_hydrophobic == True:
                     in_between_planes.append(aa.id)
-                    number_atoms_hits+=1
-                    #print("ca is under plane a and is over p2", aa)
-        
+                    number_atoms_hits += 1
+                    # print("ca is under plane a and is over p2", aa)
         if number_atoms_in_between == 0:
             print("No more atoms in between")
-            return False
-        
-        #print(f"Hit ratio {number_atoms_hits} \t {number_atoms_in_between} = {number_atoms_hits/number_atoms_in_between}")
+            return (False)
+
+        # print(f"Hit ratio {number_atoms_hits} \t {number_atoms_in_between} = {
+        # number_atoms_hits/number_atoms_in_between}")
         if number_atoms_hits > self.best_number_hits:
             # Updating the "best" match
-            # print("IIIIIIIIIIIIIIIIIIIIIIIIII")
             self.best_number_hits = number_atoms_hits
             self.best_number_aa = number_atoms_in_between
-        return True if len(in_between_planes) > 0 else False
+        return (True) if len(in_between_planes) > 0 else (False)
 
 
-def plot_plane(plane1, plane2=None, point=None):
-        X, Z = np.meshgrid(range(100), range(100))
-        # Calculate the corresponding y values for the plane
-        # mouais, plutot faire en sorte quil ne soit jamais 0
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter3D(point.get_x(), point.get_y(), point.get_z(), color="red")
-        if point is not None and plane1.d !=0 :
-            Y1 = (-plane1.a * X - plane1.c * Z - plane1.d) / plane1.b
-            ax.plot_surface(X, Y1, Z, alpha=0.2, color = "green")
+"""def plot_plane(plane1, plane2=None, point=None):
+    X, Z = np.meshgrid(range(100), range(100))
+    # Calculate the corresponding y values for the plane
+    # mouais, plutot faire en sorte quil ne soit jamais 0
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter3D(point.get_x(), point.get_y(), point.get_z(), color="red")
+    if point is not None and plane1.d != 0:
+        Y1 = (-plane1.a * X - plane1.c * Z - plane1.d) / plane1.b
+        ax.plot_surface(X, Y1, Z, alpha=0.2, color="green")
 
-        if plane2 is not None and plane2.d !=0:
-            Y2 = (-plane2.a * X - plane2.c * Z - plane2.d) / plane2.b
-            ax.plot_surface(X, Y2, Z, color = 'red', alpha=0.2)
-        plt.show()
+    if plane2 is not None and plane2.d != 0:
+        Y2 = (-plane2.a * X - plane2.c * Z - plane2.d) / plane2.b
+        ax.plot_surface(X, Y2, Z, color='red', alpha=0.2)
+    plt.show()"""
+
 
 # TODO: faire que le demi cercle
 def find_points(n_points, center_coordinates):
     points = []
-    for k in range(1,n_points+1):# n-points pour éviter division par 0 
-        h = -1 + (2 * (k-1)/(n_points - 1))
+    for k in range(1, n_points + 1):  # n-points pour éviter division par 0
+        h = -1 + (2 * (k - 1) / (n_points - 1))
         theta = math.acos(h)
-        if k==1 or k==n_points:
+        if k == 1 or k == n_points:
             phi = 0
-        else :
-            phi = (phi + (3.6/math.sqrt(n_points) * (1/math.sqrt(1-h*h)))) % (2*math.pi)
+        else:
+            phi = (phi + (3.6 / math.sqrt(n_points) * (1 / math.sqrt(1 - h * h)))) % (2 * math.pi)
         # Centering on the mass center
         x = center_coordinates.get_x() + math.sin(phi) * math.sin(theta)
         y = center_coordinates.get_y() + math.cos(theta)
         z = center_coordinates.get_z() + math.cos(phi) * math.sin(theta)
-        points.append(Point(x,y,z))
+        points.append(Point(x, y, z))
     above_x_axis_points = [point for point in points if point.get_z() > center_coordinates.get_z()]
     return above_x_axis_points
 
-def find_director_vector(point:Point, center_coordinate:Point):
+
+def find_director_vector(point: Point, center_coordinate: Point):
     """Find the vector that goes through the two points = the normal vector of the plan"""
     # The normal vector of the plan
-    return Vector(center_coordinate.get_x()-point.get_x(), center_coordinate.get_y() - point.get_y(), center_coordinate.get_z() - point.get_z())
+    return Vector(center_coordinate.get_x() - point.get_x(), center_coordinate.get_y() - point.get_y(),
+                  center_coordinate.get_z() - point.get_z())
 
 
 def find_normal_plan(director_vector):
     """Returns 2 normal vectors making a plan orthogonal to the director vector"""
-    x,y,z=director_vector
-    return ( np.array([-y,x,0]) , np.array([-z,0,x]))
+    x, y, z = director_vector
+    return (np.array([-y, x, 0]), np.array([-z, 0, x]))
 
-def caculate_solvant_accessibility(structure, input_file): # a voir pour identifier le bon aa
-        # Using DSSP
-        print("Computing solvant accessibility...")
 
-        model = structure[0]
-        dssp = DSSP(model, input_file)
-        # Store ASA values for each residue
-        asa_values = {}
+def caculate_solvant_accessibility(structure, input_file):  # a voir pour identifier le bon aa
+    # Using DSSP
+    print("Computing solvant accessibility...")
 
-        # Iterate over the residues in the structure
-        for residue in model.get_residues():
-            chain_id = residue.parent.id
-            residue_id = residue.id
-            key = (chain_id, residue_id)
-            if key in dssp:
-                asa_value = dssp[key][3]  # ASA value for the residue
-                asa_values[key] = asa_value
-        # Now, asa_values is a dictionary containing ASA values for each residue in the structure
-        return asa_values
+    model = structure[0]
+    dssp = DSSP(model, input_file)
+    # Store ASA values for each residue
+    asa_values = {}
+
+    # Iterate over the residues in the structure
+    for residue in model.get_residues():
+        chain_id = residue.parent.id
+        residue_id = residue.id
+        key = (chain_id, residue_id)
+        if key in dssp:
+            asa_value = dssp[key][3]  # ASA value for the residue
+            asa_values[key] = asa_value
+    # Now, asa_values is a dictionary containing ASA values for each residue in the structure
+    return asa_values
+
 
 if __name__ == '__main__':
     # Plot points check
@@ -198,8 +205,6 @@ if __name__ == '__main__':
 
     caculate_solvant_accessibility(structure, input_file=pdb)
 
-
-    
     """mass_center = Point(3.19,37.1,36.2)
     directions = find_points(10, mass_center)
     fig = plt.figure()
@@ -226,9 +231,3 @@ if __name__ == '__main__':
         else:
             print("Vecteur directeur nul (z), pass --- ")
         """
-
-
-
-
-
-
